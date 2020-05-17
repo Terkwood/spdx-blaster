a90fbe72b8a81d17e39030880ed2413c35f7bd91
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 12:35:53 -0400

    Use an arg

--- src/comment.rs
+++ src/comment.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Copy, Clone)]
 pub enum Comment {
     DoubleFwdSlash,
     Pound,
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
+extern crate clap;
+extern crate env_logger;
+extern crate log;
+
 pub mod comment;
 pub mod dialect;
 
+use clap::{App, Arg};
+use log::info;
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = App::new(NAME)
+        .version(VERSION)
+        .author("terkwood <38859656+Terkwood@users.noreply.github.com>")
+        .about("Writes SPDX license IDs in your (so far, rust) source files.")
+        .arg(
+            Arg::with_name("write")
+                .short("w")
+                .long("write")
+                .help("Update all relevant files with license string. If this option is ommitted, you'll simply see output")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("target")
+                .help("Sets the target directory to visit")
+                .required(false)
+                .default_value(".")
+                .takes_value( true),
+        )
+        .get_matches();
+
+    info!("Args: {:?}", args);
 }
