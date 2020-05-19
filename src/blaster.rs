530248fe30344fe116826d82904c59e40bfd423c
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-19 14:54:48 -0400

    Almost work

--- src/blaster.rs
+++ src/blaster.rs
@@ -1,3 +1,4 @@
+use log::warn;
 use std::path::Path;
 
 pub struct Opts {
@@ -10,4 +11,21 @@ impl Default for Opts {
     }
 }
 
-pub fn visit(_target: &Path, _opts: Opts) {}
+pub fn visit(target: &Path, _opts: Opts) {
+    if !is_dir(target) {
+        apply_license(target)
+    } else {
+        warn!("Skipping dir: {:?}", target)
+    }
+}
+
+/// Path::is_dir() is not guaranteed to be intuitively correct for "." and ".."
+/// See: https://github.com/rust-lang/rust/issues/45302
+/// Attribution: https://github.com/sharkdp/fd/blob/master/src/filesystem.rs
+fn is_dir(path: &Path) -> bool {
+    path.is_dir() && (path.file_name().is_some() || path.canonicalize().is_ok())
+}
+
+fn apply_license(file: &Path) {
+    todo!("no impl")
+}
