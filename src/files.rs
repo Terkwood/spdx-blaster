1874db8f945d8921291afbf70359b7c802f4905f
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-21 18:55:40 -0400

    Handle shabangs gracefully (#15)

--- src/files.rs
+++ src/files.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-use memmap::MmapOptions;
+use memmap::{MmapMut, MmapOptions};
 use std::fs::{metadata, OpenOptions};
 use std::io::Error;
 use std::path::Path;
@@ -26,13 +26,57 @@ pub fn apply_license(path: &Path, license: License, comment: Comment) -> Result<
 
     let mut map = unsafe { MmapOptions::new().map_mut(&file)? };
 
-    map[(total_len - license_line_len)..].copy_from_slice(&license_line);
-
-    map.rotate_right(license_line_len);
+    let opts = &AlterOpts {
+        total_len,
+        license_line_len,
+        license_line,
+    };
+    if file_contains_shebang(&map) {
+        alter_with_shebang(&mut map, opts)
+    } else {
+        alter_basic(&mut map, opts)
+    }
 
     map.flush()
 }
 
+const SHEBANG_LEN: usize = 2;
+const SHEBANG: &str = "#!";
+fn file_contains_shebang(map: &MmapMut) -> bool {
+    let first_few_chars = std::cmp::min(map.len(), SHEBANG_LEN);
+    std::str::from_utf8(&map[..first_few_chars]).unwrap_or_default() == SHEBANG
+}
+
+fn alter_with_shebang(map: &mut MmapMut, opts: &AlterOpts) {
+    let first_newline_pos = map
+        .iter()
+        .position(|c| c == &b'\n')
+        .map(|p| p + 1)
+        .unwrap_or_else(|| map.len());
+    let mut new_intro: Vec<u8> = map.iter().take(first_newline_pos).cloned().collect();
+    for lc in &opts.license_line {
+        new_intro.push(*lc)
+    }
+
+    map.rotate_left(first_newline_pos);
+    let new_intro_len = new_intro.len();
+
+    map[(opts.total_len - new_intro_len)..].copy_from_slice(&new_intro);
+
+    map.rotate_right(new_intro_len)
+}
+
+fn alter_basic(map: &mut MmapMut, opts: &AlterOpts) {
+    map[(opts.total_len - opts.license_line_len)..].copy_from_slice(&opts.license_line);
+
+    map.rotate_right(opts.license_line_len)
+}
+
+struct AlterOpts {
+    total_len: usize,
+    license_line_len: usize,
+    license_line: Vec<u8>,
+}
 /// Path::is_dir() is not guaranteed to be intuitively correct for "." and ".."
 /// See: https://github.com/rust-lang/rust/issues/45302
 /// Attribution: https://github.com/sharkdp/fd/blob/master/src/filesystem.rs
